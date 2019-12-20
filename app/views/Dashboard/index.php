a7a86168800c7c55b4411000e8b59057397ca4cf
Sulkifli Asmunandar <kflmattukz@gmail.com>
2019-12-20 11:46:27 +0800

    just add semantic ui to dashboard

--- app/views/Dashboard/index.php
+++ app/views/Dashboard/index.php
@@ -5,16 +5,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Admin Dashboard</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  <link rel="stylesheet" href="<?= BASEURL ?>/css/main.css">
 </head>
 <body>
   <div class="ui grid">
-    <div class="four wide column">
-      <h2 class="ui header">Admin</h2>
+    <div class="five wide column">
+      <div class="ui container segment">
+        <h3 class="ui header center aligned">Admin</h3>
+      </div>
     </div>
     <div class="eleven wide column">
-      <h3 class="ui header">Content</h3>
+      <div class="ui container segment">
+      <h3 class="ui header center aligned">Content</h3>
+      </div>
     </div>
 
-  </div>    
+  </div>
+
+  <script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script src="<?= BASEURL ?>/js/main.js"></script>    
 </body>
 </html>
